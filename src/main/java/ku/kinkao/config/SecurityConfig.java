6947135c7787acdb3dfaf2ffbe4be7e75ddd07d1
Wha Chanunya <whachanunya@gmail.com>
2023-02-22 09:23:14 +0700

    Add SecurityConfig class

--- src/main/java/ku/kinkao/config/SecurityConfig.java
+++ src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -1,22 +1,25 @@
 package ku.kinkao.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/home", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated();
+
+        return http.build();
     }
 }
