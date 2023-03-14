873830fa7b93c3e7e4de16febddc9c7569cfbc72
Wha Chanunya <whachanunya@gmail.com>
2023-03-14 13:57:29 +0700

    add lombok

--- src/main/java/ku/kinkao/model/Restaurant.java
+++ src/main/java/ku/kinkao/model/Restaurant.java
@@ -1,10 +1,15 @@
 package ku.kinkao.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Restaurant {
 
