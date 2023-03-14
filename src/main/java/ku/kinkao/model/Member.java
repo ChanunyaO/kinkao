ba8f79efb373eda992b44b2163640d836919da2f
Wha Chanunya <whachanunya@gmail.com>
2023-03-14 14:27:44 +0700

    remove unnecessary

--- src/main/java/ku/kinkao/model/Member.java
+++ src/main/java/ku/kinkao/model/Member.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.Instant;
 import java.util.UUID;
