686dd97d0dadaa1e6522f4ad88cbadc2972f34f4
Wha Chanunya <whachanunya@gmail.com>
2023-03-15 09:24:36 +0700

    Input Validation

--- src/main/java/ku/kinkao/controller/SignupController.java
+++ src/main/java/ku/kinkao/controller/SignupController.java
@@ -1,14 +1,16 @@
 package ku.kinkao.controller;
 
 import ku.kinkao.dto.SignupDto;
-import ku.kinkao.model.Member;
 import ku.kinkao.service.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class SignupController {
@@ -17,32 +19,30 @@ public class SignupController {
     private SignupService signupService;
 
     @GetMapping("/signup")
-    public String getSignupPage() {
+    public String getSignupPage(SignupDto user) {
         return "signup"; // return signup.html
     }
 
     @PostMapping("/signup")
-    public String signupMember(@ModelAttribute SignupDto member, Model model) {
+    public String signupUser(@Valid SignupDto user, BindingResult result,
+                             Model model) {
+        if (result.hasErrors())
+            return "signup";
 
         String signupError = null;
 
-        if (!signupService.isUsernameAvailable(member.getUsername())) {
+        if (!signupService.isUsernameAvailable(user.getUsername())) {
             signupError = "The username already exists.";
         }
 
         if (signupError == null) {
-            int rowsAdded = signupService.createMember(member);
-            if (rowsAdded < 0) {
-                signupError = "There was an error signing you up. Please try again.";
-            }
-        }
-
-        if (signupError == null) {
+            signupService.createMember(user);
             model.addAttribute("signupSuccess", true);
         } else {
             model.addAttribute("signupError", signupError);
         }
 
+        model.addAttribute("signupDto", new SignupDto());
         return "signup";
     }
 }
--- src/main/java/ku/kinkao/dto/SignupDto.java
+++ src/main/java/ku/kinkao/dto/SignupDto.java
@@ -1,11 +1,23 @@
 package ku.kinkao.dto;
 
 import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class SignupDto {
+
+    @NotBlank
+    @Size(min=4, message = "Username must have at least 4 characters")
     private String username;
+
+    @NotBlank
+    @Size(min=12, max=128, message = "Password must have at least 12 characters")
     private String password;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank
     private String lastName;
 }
